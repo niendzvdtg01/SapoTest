36c892c477acdb616578f5cfa563aa86408794de
niendzvdtg01 <duynien34@gmail.com>
2026-03-13 17:51:22 +0700

    add: backend

---
+++ Backend/sapo/src/main/java/com/example/sapo/SapoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sapo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SapoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SapoApplication.class, args);
+	}
+
+}
---
+++ Backend/sapo/src/test/java/com/example/sapo/SapoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sapo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SapoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
